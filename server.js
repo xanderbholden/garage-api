d0d4ea43cf198e045933373f1e92285fd3a3edd6
Xander <xanderbholden@gmail.com>
2026-05-22 18:46:42 -0600

    Added swagger middleware

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-require('dotenv').config();
-
 const express = require('express');
+const mongodb = require('./db/connect');
 const cors = require('cors');
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 
-const mongodb = require('./db/connect');
-
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get('/', (req, res) => {
-  res.send('Garage API is running');
-});
 
 app.use('/', require('./routes'));
 
