e821d3cf55ddad6dbc9d9346dce234f54de4bc30
Xander <xanderbholden@gmail.com>
2026-05-22 19:01:21 -0600

    Fix Render routes and Swagger

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const mongodb = require('./db/connect');
-const cors = require('cors');
+require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 
-const app = express();
+const mongodb = require('./db/connect');
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => {
+  res.send('Garage API is running');
+});
 
 app.use('/', require('./routes'));
 
