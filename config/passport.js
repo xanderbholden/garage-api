1f75f466c4a2b87ecf1c6725955e1286f92cd03b
Xander <xanderbholden@gmail.com>
2026-05-29 14:49:39 -0600

    Complete OAuth authentication

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: '/auth/github/callback'
+      callbackURL:
+        process.env.CALLBACK_URL || 'http://localhost:3000/auth/github/callback'
     },
     function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
