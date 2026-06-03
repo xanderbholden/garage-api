d7ffa29fc057abce79473dc837ee0b1f4d9fe830
Xander <xanderbholden@gmail.com>
2026-06-03 14:11:43 -0600

    Fix Swagger POST PUT body schemas

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -4,13 +4,47 @@ const router = express.Router();
 const vehiclesController = require('../controllers/vehicles');
 const isAuthenticated = require('../middleware/authenticate');
 
-// Public routes
 router.get('/', vehiclesController.getAll);
 router.get('/:id', vehiclesController.getSingle);
 
-// Protected routes
-router.post('/', isAuthenticated, vehiclesController.createVehicle);
-router.put('/:id', isAuthenticated, vehiclesController.updateVehicle);
+router.post('/', isAuthenticated, (req, res) => {
+  /* #swagger.parameters['body'] = {
+    in: 'body',
+    required: true,
+    schema: {
+      make: 'Subaru',
+      model: 'WRX Premium',
+      year: 2022,
+      color: 'WR Blue Pearl',
+      vin: 'JF1VBAA61N9012345',
+      mileage: 16000,
+      fuelType: 'Premium Gasoline',
+      transmission: 'Manual',
+      owner: 'Xander Holden'
+    }
+  } */
+  vehiclesController.createVehicle(req, res);
+});
+
+router.put('/:id', isAuthenticated, (req, res) => {
+  /* #swagger.parameters['body'] = {
+    in: 'body',
+    required: true,
+    schema: {
+      make: 'Subaru',
+      model: 'WRX Premium',
+      year: 2022,
+      color: 'WR Blue Pearl',
+      vin: 'JF1VBAA61N9012345',
+      mileage: 16500,
+      fuelType: 'Premium Gasoline',
+      transmission: 'Manual',
+      owner: 'Xander Holden'
+    }
+  } */
+  vehiclesController.updateVehicle(req, res);
+});
+
 router.delete('/:id', isAuthenticated, vehiclesController.deleteVehicle);
 
 module.exports = router;
